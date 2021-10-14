a40bb58f3a4c97345f2cfe843d43b18e94728214
terloo <chensijie08@gmail.com>
2021-10-14 21:57:00 +0800

    手动脏跟踪

--- src/main/java/com/github/terloo/springboot/tomcat/RedisSessionProperties.java
+++ src/main/java/com/github/terloo/springboot/tomcat/RedisSessionProperties.java
@@ -1,5 +1,7 @@
 package com.github.terloo.springboot.tomcat;
 
+import com.github.terloo.springboot.tomcat.session.redis.RedisSession;
+
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 @ConfigurationProperties(prefix = "server.servlet.session")
@@ -7,12 +9,48 @@ public class RedisSessionProperties {
 
     private String strategy = "default";
 
+    private DirtyTracking dirtyTracking;
+
     public String getStrategy() {
         return strategy;
     }
 
+    public DirtyTracking getDirtyTracking() {
+        return dirtyTracking;
+    }
+
+    public void setDirtyTracking(DirtyTracking dirtyTracking) {
+        this.dirtyTracking = dirtyTracking;
+    }
+
     public void setStrategy(String strategy) {
         this.strategy = strategy;
+    }
+
+    public static class DirtyTracking {
+
+        private Boolean enable = false;
+
+        private String flag = "__changed__";
+
+        public Boolean getEnable() {
+            return enable;
+        }
+
+        public void setEnable(Boolean enable) {
+            RedisSession.setManualDirtyTrackingSupportEnabled(enable);
+            this.enable = enable;
+        }
+
+        public String getFlag() {
+            return flag;
+        }
+
+        public void setFlag(String flag) {
+            RedisSession.setManualDirtyTrackingAttributeKey(flag);
+            this.flag = flag;
+        }
+
     }
 
 }
